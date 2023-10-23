1984963aca236e9f4d4b688171e6e753b53b3f72
Ada Rachel <adarachelofficial@gmail.com>
2023-10-23 11:55:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Allow the platform to set the port if necessary
 const quotes = require('./quotes.json'); // Import your quotes
+const cors = require('cors');
+app.use(cors());
 
 // Root path ("/") for a welcome message
 app.get('/', (req, res) => {
